e7d72bd2e4d1bd12b1a8eaf3fffa5e34fcf0b728
0918nobita <nobita.0918@gmail.com>
2020-02-15 23:06:17 +0900

    :label: Comp.layer メソッドの型定義を追加

--- src/comp.d.ts
+++ src/comp.d.ts
@@ -10,4 +10,13 @@ declare class Comp {
 
     /** Returns the composition duration, in seconds. */
     duration: number;
+
+    /** Retrives the layer by number (order in the Timeline panel). */
+    layer(index: number): Layer | Light | Camera;
+
+    /** Retrives the layer by name.
+     * Names are matched according to layer name, or source name if there is no layer name.
+     * If duplicate names exist, After Effects uses the first (topmost) one in the Timeline panel.
+     */
+    layer(name: string): Layer | Light | Camera;
 }
