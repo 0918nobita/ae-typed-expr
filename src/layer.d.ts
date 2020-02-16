cc4a6dd2c4433430bccd80cde0e34069f0acab65
0918nobita <nobita.0918@gmail.com>
2020-02-16 16:11:25 +0900

    :label: Layer.transform の型定義を追加

--- src/layer.d.ts
+++ src/layer.d.ts
@@ -1,3 +1,10 @@
+interface Transform {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    position: Property<any>;
+}
+
 declare class Layer {
     private _: 'layer';
+
+    transform: Transform;
 }
