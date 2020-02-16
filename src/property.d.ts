3691a2ee2c315363dd5117149ca1ef0e7fd29a03
0918nobita <nobita.0918@gmail.com>
2020-02-16 18:48:48 +0900

    :label: Property クラスにメンバを追加

--- src/property.d.ts
+++ src/property.d.ts
@@ -1,6 +1,27 @@
 declare class Property<T> {
     private _: 'property';
 
+    /** Returns the name of the property or property group */
+    name: string;
+
+    propertyGroup(countUp?: number): Group;
+
+    /**
+     * Returns the index of a property relative to other properties
+     * in its property group, including property groups within masks,
+     * text animators, selectors, shapes, trackers, and track points.
+     */
+    propertyIndex: number;
+
+    /**
+     * Returns a 1D, positive speed value equal to the speed
+     * at which the property is changing at the default time.
+     * This can be used only for spatial properties.
+     */
+    speed?: number;
+
+    speedAtTime?: (t: number) => number;
+
     value: T;
 
     wiggle(
